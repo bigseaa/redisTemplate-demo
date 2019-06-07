0d74529b6098bba398303ad910d36c69407f0354
bigseaa <506798278@qq.com>
2019-06-07 17:09:22 +0800

    提交

--- src/main/java/com/bigsea/service/Animal1Service.java
+++
@@ -1,9 +0,0 @@
-package com.bigsea.service;
-
-import com.bigsea.entity.Animal1;
-
-public interface Animal1Service {
-    void add(Animal1 animal1);
-
-    void addWithException(Animal1 animal1);
-}
--- src/main/java/com/bigsea/service/BootUserService.java
+++
@@ -1,7 +0,0 @@
-package com.bigsea.service;
-
-import com.bigsea.entity.BootUser;
-
-public interface BootUserService {
-    BootUser login(String name, String password);
-}
