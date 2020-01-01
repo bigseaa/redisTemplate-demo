437a9da30028ace7383d52ab889473530b3ba9fa
bigseaa <506798278@qq.com>
2020-01-01 10:35:05 +0800

    使用sorted-set实现延时队列

---
+++ src/test/java/com/bigsea/demo/delaymq/MessageListener.java
@@ -0,0 +1,53 @@
+package com.bigsea.demo.delaymq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 使用redis实现普通消息队列，实现方式使用sorted-set的数据结构
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessageListener {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 消费队列
+     */
+    @Test
+    public void listenMessage() {
+        while(true){
+            Set<ZSetOperations.TypedTuple<String>> orderIdSet = redisTemplate.opsForZSet().rangeWithScores("orderId", 0, -1);
+            if(orderIdSet == null || orderIdSet.isEmpty()){
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                continue;
+            }
+            Iterator<ZSetOperations.TypedTuple<String>> iterator = orderIdSet.iterator();
+            ZSetOperations.TypedTuple<String> next = iterator.next();
+            Double score = next.getScore();
+            if(score == null) {
+                continue;
+            }
+            double nowTime = System.currentTimeMillis();
+            if(nowTime >= score) {
+                String value = next.getValue();
+                redisTemplate.opsForZSet().remove("orderId", value);
+                System.out.println("已成功处理一条订单，订单id为" + value);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/bigsea/demo/delaymq/MessageSender.java
@@ -0,0 +1,37 @@
+package com.bigsea.demo.delaymq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Calendar;
+import java.util.Random;
+
+/**
+ * 使用redis实现普通消息队列，实现方式使用sorted-set的数据结构
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessageSender {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 向队列发送消息（往list的最右边添加元素）
+     */
+    @Test
+    public void sendMessage() {
+        // 时间戳取五秒后的数据，存入sorted-set的score值
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND, 5);
+        double millisecond = calendar.getTimeInMillis();
+        // 以简单的方式模拟订单号
+        Random random = new Random();
+        int orderId = Math.abs(random.nextInt());
+        redisTemplate.opsForZSet().add("orderId", String.valueOf(orderId), millisecond );
+        System.out.println("发送订单任务，订单ID为===============" + orderId);
+    }
+}
